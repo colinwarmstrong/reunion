6bd53b8c80a1894520638f6ce1cfb2ca540f3c8c
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-30 10:05:18 -0600

    Completes iteration 3; adds basic framework and methods for the Reunion class.

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,14 @@
+require_relative 'activity.rb'
 class Reunion
+  attr_reader :location, :activities
+  def initialize(location)
+    @location = location
+    @activities = []
+  end
+
+  def add_activity(name, base_cost = 0, cost_per_participant = 0)
+    new_activity = Activity.new(name, base_cost, cost_per_participant)
+    @activities << new_activity
+    return new_activity
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,30 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def test_it_exists
+    reunion = Reunion.new('Dark side of the moon')
+
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_reunions_have_a_location
+    reunion = Reunion.new('South Dakota: the lesser of the two Dakotas')
+
+    assert_equal 'South Dakota: the lesser of the two Dakotas', reunion.location
+  end
+
+  def test_it_can_add_activities
+    reunion = Reunion.new("Davy Jones' locker")
+
+    assert_equal [], reunion.activities
+
+    safari = reunion.add_activity('safari adventure')
+    assert_equal [safari], reunion.activities
+
+    bowling = reunion.add_activity('extreme bowling adventure')
+    assert_equal [safari, bowling], reunion.activities
+
+    knitting = reunion.add_activity('ultra extreme ballistic knitting adventure')
+    assert_equal [safari, bowling, knitting], reunion.activities
+  end
 end
