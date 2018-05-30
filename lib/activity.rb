4845c9c24d6acf5d98148e663c0e675d283d471b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-30 09:30:55 -0600

    Completes iteration 1; adds basic framework and methods for the Activity class.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,19 @@
 class Activity
+  attr_reader :name, :participants, :base_cost, :cost_per_participant
+
+  def initialize(name, base_cost, cost_per_participant)
+    @name = name
+    @participants = {}
+    @base_cost = base_cost
+    @cost_per_participant = cost_per_participant
+  end
+
+  def add_participant(participant)
+    @participants[participant] = 0
+  end
+
+  def total_cost
+    @cost_per_participant * @participants.length + @base_cost
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,6 +2,49 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
-
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new('snorkeling', 200, 12)
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_activities_are_initialize_with_a_name_and_empty_participants_hash
+    activity = Activity.new('loitering', 5000, 6)
+
+    assert_equal 'loitering', activity.name
+    assert_equal ({}), activity.participants
+  end
+
+  def test_activities_are_initialize_with_a_base_cost_and_cost_per_participant
+    activity = Activity.new('light jogging', 70, 900)
+
+    assert_equal 70, activity.base_cost
+    assert_equal 900, activity.cost_per_participant
+  end
+
+  def test_it_can_add_participants
+    activity = Activity.new('interpretive dance', -12, 16.4)
+
+    assert_equal 0, activity.participants.length
+    activity.add_participant('Billy Bob')
+    assert_equal 1, activity.participants.length
+    activity.add_participant('Billy Joe')
+    assert_equal 2, activity.participants.length
+    activity.add_participant('Billy Jean')
+    assert_equal 3, activity.participants.length
+  end
+
+  def test_we_can_find_total_cost
+    activity = Activity.new('running for mayor', 1, 1_000_000_000)
+    activity.add_participant('Billy Goat')
+    expected = activity.total_cost
+
+    assert_equal 1_000_000_001, expected
+
+    activity.add_participant('Billy Boy')
+    expected = activity.total_cost
+
+    assert_equal 2_000_000_001, expected
+  end
 end
